b79a14c1905c62c31cdb58c1acec8e11215a7668
Игорь Голяк <golyak.03@inbox.ru>
2021-04-22 08:59:59 +0700

    22.04.21

---
+++ 22.04.21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._04._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите цело число");
+            int k = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(k + " ");
+            }
+            Console.WriteLine();
+            for (int i=5; i <=20; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
